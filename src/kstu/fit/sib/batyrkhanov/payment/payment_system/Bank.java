2292db191f92e3ed23ed6de838a52440ad4442b1
Philinnnn <159049463+Philinnnn@users.noreply.github.com>
2024-04-15 23:25:43 +0500

    Update Bank.java

--- src/kstu/fit/sib/batyrkhanov/payment/payment_system/Bank.java
+++ src/kstu/fit/sib/batyrkhanov/payment/payment_system/Bank.java
@@ -1,7 +1,9 @@
 package kstu.fit.sib.batyrkhanov.payment.payment_system;
 
 import kstu.fit.sib.batyrkhanov.payment.utilities.Menu;
-
+/*
+git push -u origin main
+*/
 public class Bank {
     public static void main(String[] args) {
         Menu.start();
